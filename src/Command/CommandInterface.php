89867bf9c4e433479e1fc66d23709bf17bf4706b
Aiga Chu <aigachu.sama@gmail.com>
2018-06-14 02:59:57 -0400

    Added listener and command interfaces.

---
+++ src/Command/CommandInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Lavenza
+ * Copyright 2017-2018 Aigachu, All Rights Reserved
+ *
+ * License: https://github.com/Aigachu/Lavenza/blob/master/LICENSE
+ */
+
+namespace Aigachu\Lavenza\Command;
+
+/**
+ * Interface CommandInterface
+ * @package Aigachu\Lavenza\Command
+ */
+interface CommandInterface
+{
+
+}
---
+++ src/Listener/ListenerInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Lavenza
+ * Copyright 2017-2018 Aigachu, All Rights Reserved
+ *
+ * License: https://github.com/Aigachu/Lavenza/blob/master/LICENSE
+ */
+
+namespace Aigachu\Lavenza\Listener;
+
+/**
+ * Interface ListenerInterface
+ * @package Aigachu\Lavenza\Listener
+ */
+interface ListenerInterface
+{
+
+}
